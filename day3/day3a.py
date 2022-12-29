84a2ad762a23b0f9a7e929b5e4dc08dc585ffdf9
Henri Heilala <contact@henriheilala.fi>
2022-12-29 13:27:19 +0200

    refactor

--- day3/day3a.py
+++ day3/day3a.py
@@ -3,20 +3,20 @@ import string
 with open('input3.txt', 'r') as file:
     inventory = file.read().split('\n')
 
-items = list(string.ascii_letters)
+item_types = list(string.ascii_letters)
 priorities = list(range(1,53))
 
-values = {}
-points = 0
+item_priorities = {}
+sum_of_prio = 0
 
-for key, value in zip(items, priorities):
-    values[key] = value
+for key, value in zip(item_types, priorities):
+    item_priorities[key] = value
 
 for rucksack in inventory:
     first_bag = rucksack[:int(len(rucksack)/2)]
     second_bag = rucksack[int(len(rucksack)/2):]
 
     common_item = ''.join(set(first_bag).intersection(second_bag))
-    points += values[common_item]
+    sum_of_prio += item_priorities[common_item]
 
-print(points)
+print(sum_of_prio)
