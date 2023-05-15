57a22dc4958c3b42e333b9d47c04d24f17068798
Henri Heilala <contact@henriheilala.fi>
2023-05-15 14:42:25 +0300

    day4a refactor

--- day4/day4a.py
+++ day4/day4a.py
@@ -1,10 +1,15 @@
 with open('input4.txt', 'r') as file:
     task_list = file.read().split('\n')
 
+def range_within(range1, range2):
+    x1, x2 = range1.start, range1.stop
+    y1, y2 = range2.start, range2.stop
+    return (x1 <= y1 and y2 <= x2) or (x1 >= y1 and  x2 <= y2)
+
 task_list_split = [task.split(',') for task in task_list]
 task_list_split = [[task[0].split('-'), task[1].split('-')] for task in task_list_split] 
 task_list_ranges = [[range(int(task[0][0]), int(task[0][1])), range(int(task[1][0]), int(task[1][1]))] for task in task_list_split] 
-task_list_result = [True if range_pair[0].start <= range_pair[1].start and range_pair[0].stop >= range_pair[1].stop or range_pair[0].start >= range_pair[1].start and range_pair[0].stop <= range_pair[1].stop else False for range_pair in task_list_ranges]
+task_list_result = [True if range_within(range_pair[0], range_pair[1]) else False for range_pair in task_list_ranges]
 
 
 result = task_list_result.count(True)
