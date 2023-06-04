07f3cf6fe86e8ba62e09bc6ccc8892b4c79e8019
Henri Heilala <contact@henriheilala.fi>
2023-06-04 18:56:50 +0300

    refactor: day8

--- day8/day8a.py
+++ day8/day8a.py
@@ -1,5 +1,4 @@
-with open('input8.txt', 'r') as file:
-    tree_map = [line.strip() for line in file.readlines()]
+tree_map = [[int(height) for height in line.strip()] for line in open('input8.txt', 'r')]
 
 VISIBLE_TREES = 0
 
@@ -7,28 +6,28 @@ def check_visibility(tree_x, tree_y, tree_height):
     global VISIBLE_TREES
 
     for position in range(tree_x): #west
-        if int(tree_map[tree_y][position]) >= tree_height:
+        if tree_map[tree_y][position] >= tree_height:
             break
         if position == tree_x-1:
             VISIBLE_TREES += 1
             return
     
     for position in range(tree_x + 1, len(tree_map[0])): #east
-        if int(tree_map[tree_y][position]) >= tree_height:
+        if tree_map[tree_y][position] >= tree_height:
             break
         if position == len(tree_map[0])-1:
             VISIBLE_TREES += 1
             return
 
     for position in range(tree_y): #north
-        if int(tree_map[position][tree_x]) >= tree_height:
+        if tree_map[position][tree_x] >= tree_height:
             break
         if position == tree_y-1:
             VISIBLE_TREES += 1
             return
 
     for position in range(tree_y + 1, len(tree_map)): #south
-        if int(tree_map[position][tree_x]) >= tree_height:
+        if tree_map[position][tree_x] >= tree_height:
             break
         if position == len(tree_map)-1:
             VISIBLE_TREES += 1
@@ -42,7 +41,7 @@ for y in range(len(tree_map)):
         if x == len(tree_map[0])-1 or y == len(tree_map)-1:
             VISIBLE_TREES += 1
             continue
-        check_visibility(x, y, int(tree_map[y][x]))
+        check_visibility(x, y, tree_map[y][x])
 
 RESULT = VISIBLE_TREES
 print('result: {RESULT}'.format(RESULT=RESULT))
--- day8/day8b.py
+++ day8/day8b.py
@@ -1,5 +1,4 @@
-with open('input8.txt', 'r') as file:
-    tree_map = [line.strip() for line in file.readlines()]
+tree_map = [[int(height) for height in line.strip()] for line in open('input8.txt', 'r')]
 
 SCENIC_SCORE_LIST = []
 
@@ -7,28 +6,28 @@ def calc_scenic_score(tree_x, tree_y, tree_height):
     score_west, score_east, score_north, score_south = 0, 0, 0, 0
 
     for position in range(tree_x - 1, -1, -1): #west
-        if int(tree_map[tree_y][position]) >= tree_height:
+        if tree_map[tree_y][position] >= tree_height:
             score_west += 1
             break
         else:
             score_west += 1
 
     for position in range(tree_x + 1, len(tree_map[0])): #east
-        if int(tree_map[tree_y][position]) >= tree_height:
+        if tree_map[tree_y][position] >= tree_height:
             score_east += 1
             break
         else:
             score_east += 1
 
     for position in range(tree_y - 1 , -1, -1): #north
-        if int(tree_map[position][tree_x]) >= tree_height:
+        if tree_map[position][tree_x] >= tree_height:
             score_north += 1
             break
         else:
             score_north += 1
 
     for position in range(tree_y + 1, len(tree_map)): #south
-        if int(tree_map[position][tree_x]) >= tree_height:
+        if tree_map[position][tree_x] >= tree_height:
             score_south += 1
             break
         else:
@@ -38,7 +37,7 @@ def calc_scenic_score(tree_x, tree_y, tree_height):
 
 for y in range(len(tree_map)):
     for x in range(len(tree_map[0])):
-        calc_scenic_score(x, y, int(tree_map[y][x]))
+        calc_scenic_score(x, y, tree_map[y][x])
 
 RESULT = max(SCENIC_SCORE_LIST)
 print('result: {RESULT}'.format(RESULT=RESULT))
